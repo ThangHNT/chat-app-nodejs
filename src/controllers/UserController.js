2bee40a3d594b53052e939fe6b179d48d7d746b0
ThangHNT <thanghn2503@gmail.com>
2022-08-07 15:38:18 +0700

    model, controller, routes

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,10 +1,49 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
 class UserController {
     home(req, res, next) {
         res.send('server running');
     }
 
-    register(req, res, next) {
-        console.log(req.body);
+    async login(req, res, next) {
+        try {
+            const { username, password } = req.body;
+            const hashPassword = await bcrypt.hash(password, 10);
+            const userCheck = await User.findOne({ username, hashPassword });
+            if (!userCheck) {
+                return res.json({ msg: 'Tài khoản hoặc mật khẩu không đúng.', status: false });
+            } else {
+                return res.json({ status: true, user: userCheck });
+            }
+        } catch (err) {
+            console.log('login that bai');
+        }
+    }
+
+    async register(req, res, next) {
+        try {
+            const { username, email, password } = req.body;
+            const usernameCheck = await User.findOne({ username });
+            if (usernameCheck) {
+                return res.json({ msg: 'Tên người dùng đã tồn tại', status: false });
+            }
+            const emailCheck = await User.findOne({ email });
+            if (emailCheck) {
+                return res.json({ msg: 'Email đã được đăng ký', status: false });
+            }
+            const hashPassword = await bcrypt.hash(password, 10);
+            const user = new User({
+                username,
+                password: hashPassword,
+                email,
+            });
+            user.save();
+            delete user.password;
+            return res.json({ status: true, user });
+        } catch (e) {
+            console.log('loi dang ky');
+        }
     }
 }
 
--- src/models/user.js
+++ src/models/user.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const User = new Schema({
     username: { type: String, maxLength: 30, required: true, unique: true, minLength: 3 },
-    password: { type: String, maxLength: 30, required: true, minLength: 8 },
-    email: { type: String, maxLength: 30, minLength: 10 },
+    password: { type: String, maxLength: 100, required: true, minLength: 1 },
+    email: { type: String, maxLength: 30, minLength: 5 },
     avatar: { type: String },
 });
 
