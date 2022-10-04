9e130b699c8402298923765c715c5003e7e5f7e8
ThangHNT <thanghn2503@gmail.com>
2022-10-04 23:42:23 +0700

    revoked and remove msg modal

--- src/controllers/MessageController.js
+++ src/controllers/MessageController.js
@@ -1,4 +1,19 @@
 const Message = require('../models/message');
+const User = require('../models/user');
+
+const checkMessageDeleted = (arr, userId) => {
+    const ans = [];
+    arr.forEach((msg) => {
+        if (msg.userDeletedMessage) {
+            const checkMsg = msg.userDeletedMessage.some((id) => {
+                return id != userId;
+            });
+            if (!checkMsg) ans.push(msg);
+        }
+    });
+    return ans;
+};
+
 class MessageController {
     getMessages(req, res, next) {
         const { sender, receiver } = req.body;
@@ -6,7 +21,8 @@ class MessageController {
         Message.find({ sender: sender, receiver: receiver }, function (err, messages) {
             Message.find({ sender: receiver, receiver: sender }, function (err, messages2) {
                 if (messages.length > 0 || messages2.length > 0) {
-                    const data1 = messages.map((item) => {
+                    const arr1 = checkMessageDeleted(messages, sender);
+                    const data1 = arr1.map((item) => {
                         const obj = {
                             id: item._id,
                             type: item.type,
@@ -19,7 +35,8 @@ class MessageController {
                         };
                         return obj;
                     });
-                    const data2 = messages2.map((item) => {
+                    const arr2 = checkMessageDeleted(messages2, sender);
+                    const data2 = arr2.map((item) => {
                         const obj = {
                             id: item._id,
                             type: item.type,
@@ -51,6 +68,7 @@ class MessageController {
             const message = new Message();
             message.sender = sender;
             message.receiver = receiver;
+            message.userDeletedMessage = [];
             const type = msg.type;
             if (msg.type === 'text') {
                 message.type = 'text';
@@ -109,13 +127,15 @@ class MessageController {
         const { receiver, sender } = req.body;
         Message.find({ sender, receiver }, function (err, messages) {
             Message.find({ sender: receiver, receiver: sender }, function (err, messages2) {
-                // console.log(messages == null, messages2);
                 if (messages.length > 0 || messages2.length > 0) {
-                    let n = messages.length;
-                    let m = messages2.length;
-                    let a = messages[n - 1];
-                    let b = messages2[m - 1];
-                    if (a && b) {
+                    // console.log(checkMessageDeleted(messages));
+                    const arr = checkMessageDeleted(messages, sender);
+                    const arr2 = checkMessageDeleted(messages2, sender);
+                    let n = arr.length;
+                    let m = arr2.length;
+                    let a = arr[n - 1];
+                    let b = arr2[m - 1];
+                    if (arr.length > 0 && arr2.length > 0) {
                         let msg = '';
                         let time1 = a.createdAt.getTime();
                         let time2 = b.createdAt.getTime();
@@ -128,14 +148,14 @@ class MessageController {
                             };
                         }
                         return res.json({ status: true, message: msg });
-                    } else if (a) {
+                    } else if (arr.length > 0) {
                         let time = a.createdAt.getTime();
                         let msg = {
                             message: a,
                             time,
                         };
                         return res.json({ status: true, message: msg });
-                    } else if (b) {
+                    } else if (arr2.length > 0) {
                         let time = b.createdAt.getTime();
                         let msg = {
                             message: b,
@@ -150,13 +170,33 @@ class MessageController {
         });
     }
 
-    deleteMessages(req, res) {
+    deleteAllMessages(req, res) {
         return res.json({ status: true });
     }
 
-    revokedMessage(req, res) {
-        console.log(req.body);
-        return res.send('oke');
+    revokeMessage(req, res) {
+        // console.log(req.body);
+        const { messageId, action, userId, type } = req.body;
+        Message.findOne({ _id: messageId }, function (err, message) {
+            if (message) {
+                if (action == 'revoke' && type != 'revoked') {
+                    console.log('thu hoi tin nhan');
+                    message.type = 'revoked';
+                    message.text = 'Tin nhắn đã bị thu hồi';
+                    // message.save();
+                    return res.json({ status: true, msg: 'thu hồi tin nhắn thành công' });
+                } else {
+                    // console.log('xoa tn');
+                    if (message.userDeletedMessage.length == 0) {
+                        message.userDeletedMessage.push(userId);
+                        // message.save();
+                    } else {
+                        // message.remove();
+                    }
+                    return res.json({ status: true, msg: 'Xóa tin nhắn thành công' });
+                }
+            }
+        });
     }
 }
 
--- src/routes/message.js
+++ src/routes/message.js
@@ -4,10 +4,10 @@ const messageController = require('../controllers/MessageController.js');
 
 router.post('/api/send/reaction-icon', messageController.sendReactionIcon); // gửi reaction icon
 router.post('/api/remove/reaction-icon', messageController.removeReactionIcon); // xóa reaction icon
-router.post('/api/delete-messages', messageController.deleteMessages); // xóa đoạn chat
+router.post('/api/delete-all-messages', messageController.deleteAllMessages); // xóa đoạn chat
 router.post('/api/send/message', messageController.sendMessage); // gửi tin nhắn
 router.post('/api/lastest-message', messageController.getLastestMessage); // lấy tin nhắn mới nhất đã gửi
 router.post('/api/get/messages', messageController.getMessages); // lấy tất cả tin nhắn đã gửi
-router.post('/api/revoked-message', messageController.revokedMessage); // gỡ bỏ tin nhắn
+router.post('/api/revoke-message', messageController.revokeMessage); // gỡ bỏ tin nhắn
 
 module.exports = router;
